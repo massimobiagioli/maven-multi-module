061d06ab401f2e0329fb08489fe2d94ebed18c58
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-02-04 19:13:56 +0100

    Accesso al backend da frontend

--- frontend/src/main/java/id/demo/bean/DummyBean.java
+++ frontend/src/main/java/id/demo/bean/DummyBean.java
@@ -3,6 +3,10 @@ package id.demo.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 @ManagedBean(name = "dummyBean")
 @RequestScoped
 public class DummyBean {
@@ -10,7 +14,18 @@ public class DummyBean {
 	private String msg;
 	
 	public DummyBean() {
-		this.setMsg("Messaggio dummy");
+		this.setMsg(this.readDummyValueFromBackend());
+	}
+	
+	private String readDummyValueFromBackend() {
+		String backendDummyUrl = "http://localhost:8080/backend/rest/dummy/hello/pippo";
+		try {
+			HttpResponse<String> response = Unirest.get(backendDummyUrl).asString();
+			return response.getBody();
+		} catch (UnirestException e) {
+			e.printStackTrace();
+			return "*** ERRORE CHIAMATA BACKEND ***";
+		}
 	}
 	
 	public String getMsg() {
